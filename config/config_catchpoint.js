077e8806172e949ae54fd9b5c51c70fbb6c70f64
pavan485 <76582086+pavan485@users.noreply.github.com>
2022-01-16 11:07:49 +0530

    Update config_catchpoint.js

--- config/config_catchpoint.js
+++ config/config_catchpoint.js
@@ -1,7 +1,7 @@
 var config = 
 {
-    client_key: 'RW-gX-jZq1A_t8_x',
-    client_secret: '6e1a1aa6-da68-4f66-a547-1895ae30841b',
+    client_key: 'xxxxx',
+    client_secret: 'xxxxxxxx',
     base_url: 'https://io.catchpoint.com/ui/',
     token_path: 'api/token',
     last_raw_path: 'api/v1/performance/raw?tests=',
